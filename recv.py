6b7a390737f412b99cd1234fe96748c882425b5a
Hao Liang <hao.liang@shmastermatrix.com>
2024-07-23 19:09:01 +0800

    [feat] update recv.py to support mm_tool_PublishObs
    Signed-off-by: Hao Liang <hao.liang@shmastermatrix.com>

--- recv.py
+++ recv.py
@@ -7,19 +7,26 @@ import zmq
 import json
 import math
 
+import numpy as np
+
+
 class OBB:
-    def __init__(self, type, position, rotation, size):
+    def __init__(self, type, position, rotation, size, collision):
         self.type = type
         self.position = Vector3(position)
-        self.rotation = Vector3(rotation)
+        self.rotation = rotation
         self.size = Vector3(size)
-        self.color = (1, 1, 1, 1)  # Default white
+        # Default white
+        self.color = (1, 1, 1, 1)
+        self.collision = collision
 
-def draw_wire_cube(size=1.0):
+
+def draw_wire_cube(size=1.0, color=(1, 1, 1)):
     half_size = size / 2
     glBegin(GL_LINES)
-    glColor3f(1, 1, 1)  # 白色线框
-    
+    #  glColor3f(1, 1, 1)  # 白色线框
+    glColor3f(*color)
+
     # 前面
     glVertex3f(-half_size, -half_size, half_size)
     glVertex3f(half_size, -half_size, half_size)
@@ -29,7 +36,7 @@ def draw_wire_cube(size=1.0):
     glVertex3f(-half_size, half_size, half_size)
     glVertex3f(-half_size, half_size, half_size)
     glVertex3f(-half_size, -half_size, half_size)
-    
+
     # 后面
     glVertex3f(-half_size, -half_size, -half_size)
     glVertex3f(half_size, -half_size, -half_size)
@@ -39,7 +46,7 @@ def draw_wire_cube(size=1.0):
     glVertex3f(-half_size, half_size, -half_size)
     glVertex3f(-half_size, half_size, -half_size)
     glVertex3f(-half_size, -half_size, -half_size)
-    
+
     # 连接前后面
     glVertex3f(-half_size, -half_size, half_size)
     glVertex3f(-half_size, -half_size, -half_size)
@@ -49,23 +56,48 @@ def draw_wire_cube(size=1.0):
     glVertex3f(half_size, half_size, -half_size)
     glVertex3f(-half_size, half_size, half_size)
     glVertex3f(-half_size, half_size, -half_size)
-    
+
     glEnd()
+
+
+def quaternion_to_matrix(q):
+    w, x, y, z = q
+    r = [
+        1 - 2 * y * y - 2 * z * z,
+        2 * x * y - 2 * z * w,
+        2 * x * z + 2 * y * w,
+        0,
+        2 * x * y + 2 * z * w,
+        1 - 2 * x * x - 2 * z * z,
+        2 * y * z - 2 * x * w,
+        0,
+        2 * x * z - 2 * y * w,
+        2 * y * z + 2 * x * w,
+        1 - 2 * x * x - 2 * y * y,
+        0,
+        0,
+        0,
+        0,
+        1,
+    ]
+    return np.array(r).reshape(4, 4).T
+
 
 def draw_obb(obb):
     glPushMatrix()
     glTranslatef(*obb.position)
-    glRotatef(obb.rotation.x, 1, 0, 0)
-    glRotatef(obb.rotation.y, 0, 1, 0)
-    glRotatef(obb.rotation.z, 0, 0, 1)
+    # 注意：OpenGL 使用列主序，而 numpy 数组是行主序，所以我们需要转置矩阵
+    rotation = obb.rotation.flatten()
+    glMultMatrixf(rotation)
     glColor4f(*obb.color)
     glScale(*obb.size)
     #  glutWireCube(1)
     #  draw_wire_cube()
-    draw_wire_cube(1.0)  # 绘制单位立方体
+    draw_wire_cube(1.0, obb.color)  # 绘制单位立方体
     glScalef(*obb.size)  # 缩放到 OBB 的实际大小
 
     glPopMatrix()
+
 
 def draw_coordinate_system():
     glBegin(GL_LINES)
@@ -83,10 +115,12 @@ def draw_coordinate_system():
     glVertex3f(0, 0, 1)
     glEnd()
 
+
 def check_collision(obb1, obb2):
     # Simplified collision check (just checking distance)
     distance = obb1.position.distance_to(obb2.position)
     return distance < max(obb1.size.length(), obb2.size.length())
+
 
 def resize(width, height):
     print(f"resize to {width} * {height}")
@@ -100,6 +134,7 @@ def resize(width, height):
     # Add following two expression to ensure display well
     glLoadIdentity()
     glTranslatef(0.0, 0.0, -5)
+
 
 def event_handler(scale, rotation):
     global dragging
@@ -130,16 +165,28 @@ def event_handler(scale, rotation):
         elif event.type == VIDEORESIZE:
             resize(event.w, event.h)
 
+
 def recv_obb(socket, obbs):
     try:
         message = socket.recv(flags=zmq.NOBLOCK)
         data = json.loads(message)
-        obbs[:] = [OBB(obb['type'], obb['position'], obb['rotation'], obb['size']) for obb in data]
+        obbs[:] = [
+            OBB(
+                obb["type"],
+                obb["position"],
+                quaternion_to_matrix(obb["rotation"]),
+                obb["size"],
+                obb["collision_status"],
+            )
+            for obb in data
+        ]
     except zmq.Again:
         pass  # No message available
 
+
 dragging = False
 last_pos = None
+
 
 def main():
     pygame.init()
@@ -176,21 +223,22 @@ def main():
 
         draw_coordinate_system()
 
-        a_obb = next((obb for obb in obbs if obb.type == 'A'), None)
-        if a_obb:
-            for obb in obbs:
-                if obb.type == 'B':
-                    if check_collision(a_obb, obb):
-                        obb.color = (1, 0, 0, 1)  # Red
-                    else:
-                        obb.color = (0, 1, 0, 1)  # Green
-
         for obb in obbs:
+            if obb.type == "spreader" or obb.type == "sprCntr":
+                obb.color = (1, 1, 1, 1)
+            elif obb.type == "obs":
+                if obb.collision == 1:
+                    obb.color = (1, 1, 0, 1)  # Red
+                elif obb.collision == 2:
+                    obb.color = (1, 0, 0, 1)  # Red
+                else:
+                    obb.color = (1, 1, 1, 1)
             draw_obb(obb)
 
         glPopMatrix()
         pygame.display.flip()
         clock.tick(60)
+
 
 if __name__ == "__main__":
     main()
